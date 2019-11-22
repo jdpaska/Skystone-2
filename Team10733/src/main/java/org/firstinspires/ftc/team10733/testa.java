4c5b6e0d91d428d28624a91c0e0e7463f91b6aa3
CapitolHillFTC <CapitolHillFTC@gmail.com>
2019-11-22 09:10:26 -0600

    2 auton modes

--- Team10733/src/main/java/org/firstinspires/ftc/team10733/testa.java
+++ Team10733/src/main/java/org/firstinspires/ftc/team10733/testa.java
@@ -13,30 +13,30 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name="To the Line by Center", group="chad")
-public class AutoBlueByCenter extends LinearOpMode {
-        //
-        DcMotor frontleft;
-        DcMotor frontright;
-        DcMotor backleft;
-        DcMotor backright;
-        //28 * 20 / (2ppi * 4.125)
-        Double width = 16.0; //inches
-        Integer cpr = 28; //counts per rotation
-        Integer gearratio = 20;
-        Double diameter = 4.125;
-        Double cpi = (cpr * gearratio)/(Math.PI * diameter); //counts per inch, 28cpr * gear ratio / (2 * pi * diameter (in inches, in the center))
-        Double bias = 1.2;//default 0.8
-        Double meccyBias = 0.9;//change to adjust only strafing movement
-        //
-        Double conversion = cpi * bias;
-        Boolean exit = false;
-        //
-        BNO055IMU imu;
-        Orientation angles;
-        Acceleration gravity;
-//
-public void runOpMode(){
+@Autonomous(name="testa", group="chad")
+public class testa extends LinearOpMode {
+    //
+    DcMotor frontleft;
+    DcMotor frontright;
+    DcMotor backleft;
+    DcMotor backright;
+    //28 * 20 / (2ppi * 4.125)
+    Double width = 16.0; //inches
+    Integer cpr = 28; //counts per rotation
+    Integer gearratio = 20;
+    Double diameter = 4.125;
+    Double cpi = (cpr * gearratio)/(Math.PI * diameter); //counts per inch, 28cpr * gear ratio / (2 * pi * diameter (in inches, in the center))
+    Double bias = 1.2;//default 0.8
+    Double meccyBias = 0.9;//change to adjust only strafing movement
+    //
+    Double conversion = cpi * bias;
+    Boolean exit = false;
+    //
+    BNO055IMU imu;
+    Orientation angles;
+    Acceleration gravity;
+    //
+    public void runOpMode(){
         //
         initGyro();
         //
@@ -45,25 +45,23 @@ public void runOpMode(){
         backleft = hardwareMap.dcMotor.get("backLeft");
         backright = hardwareMap.dcMotor.get("backRight");
 
-        frontright.setDirection(DcMotorSimple.Direction.FORWARD);//If your robot goes backward, switch this from right to left CORRECT
-        backright.setDirection(DcMotorSimple.Direction.REVERSE);//If your robot goes backward, switch this from right to left
-    backleft.setDirection(DcMotorSimple.Direction.FORWARD);//If your robot goes backward, switch this from right to left
-    frontleft.setDirection(DcMotorSimple.Direction.REVERSE);//If your robot goes backward, switch this from right to left
-
-    //
+        frontright.setDirection(DcMotorSimple.Direction.FORWARD);
+        backright.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
+        //
         waitForStartify();
         //
-        moveToPosition(21, 0.2);
+        moveToPosition(24.2, 0.2);
         //
-        strafeToPosition(-21.0, 0.2);
+        strafeToPosition(-17.6, 0.2);
         //
-        }
-//
+    }
+    //
     /*
     This function's purpose is simply to drive forward or backward.
     To drive backward, simply make the inches input negative.
      */
-public void moveToPosition(double inches, double speed){
+    public void moveToPosition(double inches, double speed){
         //
         int move = (int)(Math.round(inches*conversion));
         //
@@ -83,25 +81,26 @@ public void moveToPosition(double inches, double speed){
         backright.setPower(speed);
         //
         while (frontleft.isBusy() && frontright.isBusy() && backleft.isBusy() && backright.isBusy()){
-        if (exit){
+            if (exit){
+                frontright.setPower(0);
+                frontleft.setPower(0);
+                backright.setPower(0);
+                backleft.setPower(0);
+                return;
+            }
+        }
         frontright.setPower(0);
         frontleft.setPower(0);
         backright.setPower(0);
         backleft.setPower(0);
         return;
-        }
-        }
-        frontright.setPower(0);
-        frontleft.setPower(0);
-        backright.setPower(0);
-        backleft.setPower(0);
-        }
-//
+    }
+    //
     /*
     This function uses the Expansion Hub IMU Integrated Gyro to turn a precise number of degrees (+/- 5).
     Degrees should always be positive, make speedDirection negative to turn left.
      */
-public void turnWithGyro(double degrees, double speedDirection){
+    public void turnWithGyro(double degrees, double speedDirection){
         //<editor-fold desc="Initialize">
         angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         double yaw = -angles.firstAngle;//make this negative
@@ -117,26 +116,26 @@ public void turnWithGyro(double degrees, double speedDirection){
         //</editor-fold>
         //
         if (speedDirection > 0){//set target positions
-        //<editor-fold desc="turn right">
-        if (degrees > 10){
-        first = (degrees - 10) + devertify(yaw);
-        second = degrees + devertify(yaw);
+            //<editor-fold desc="turn right">
+            if (degrees > 10){
+                first = (degrees - 10) + devertify(yaw);
+                second = degrees + devertify(yaw);
+            }else{
+                first = devertify(yaw);
+                second = degrees + devertify(yaw);
+            }
+            //</editor-fold>
         }else{
-        first = devertify(yaw);
-        second = degrees + devertify(yaw);
-        }
-        //</editor-fold>
-        }else{
-        //<editor-fold desc="turn left">
-        if (degrees > 10){
-        first = devertify(-(degrees - 10) + devertify(yaw));
-        second = devertify(-degrees + devertify(yaw));
-        }else{
-        first = devertify(yaw);
-        second = devertify(-degrees + devertify(yaw));
-        }
-        //
-        //</editor-fold>
+            //<editor-fold desc="turn left">
+            if (degrees > 10){
+                first = devertify(-(degrees - 10) + devertify(yaw));
+                second = devertify(-degrees + devertify(yaw));
+            }else{
+                first = devertify(yaw);
+                second = devertify(-degrees + devertify(yaw));
+            }
+            //
+            //</editor-fold>
         }
         //
         //<editor-fold desc="Go to position">
@@ -146,26 +145,26 @@ public void turnWithGyro(double degrees, double speedDirection){
         turnWithEncoder(speedDirection);
         //
         if (Math.abs(firsta - firstb) < 11) {
-        while (!(firsta < yaw && yaw < firstb) && opModeIsActive()) {//within range?
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        gravity = imu.getGravity();
-        yaw = -angles.firstAngle;
-        telemetry.addData("Position", yaw);
-        telemetry.addData("first before", first);
-        telemetry.addData("first after", convertify(first));
-        telemetry.update();
-        }
+            while (!(firsta < yaw && yaw < firstb) && opModeIsActive()) {//within range?
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+                yaw = -angles.firstAngle;
+                telemetry.addData("Position", yaw);
+                telemetry.addData("first before", first);
+                telemetry.addData("first after", convertify(first));
+                telemetry.update();
+            }
         }else{
-        //
-        while (!((firsta < yaw && yaw < 180) || (-180 < yaw && yaw < firstb)) && opModeIsActive()) {//within range?
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        gravity = imu.getGravity();
-        yaw = -angles.firstAngle;
-        telemetry.addData("Position", yaw);
-        telemetry.addData("first before", first);
-        telemetry.addData("first after", convertify(first));
-        telemetry.update();
-        }
+            //
+            while (!((firsta < yaw && yaw < 180) || (-180 < yaw && yaw < firstb)) && opModeIsActive()) {//within range?
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+                yaw = -angles.firstAngle;
+                telemetry.addData("Position", yaw);
+                telemetry.addData("first before", first);
+                telemetry.addData("first after", convertify(first));
+                telemetry.update();
+            }
         }
         //
         Double seconda = convertify(second - 5);//175
@@ -174,28 +173,28 @@ public void turnWithGyro(double degrees, double speedDirection){
         turnWithEncoder(speedDirection / 3);
         //
         if (Math.abs(seconda - secondb) < 11) {
-        while (!(seconda < yaw && yaw < secondb) && opModeIsActive()) {//within range?
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        gravity = imu.getGravity();
-        yaw = -angles.firstAngle;
-        telemetry.addData("Position", yaw);
-        telemetry.addData("second before", second);
-        telemetry.addData("second after", convertify(second));
-        telemetry.update();
-        }
-        while (!((seconda < yaw && yaw < 180) || (-180 < yaw && yaw < secondb)) && opModeIsActive()) {//within range?
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        gravity = imu.getGravity();
-        yaw = -angles.firstAngle;
-        telemetry.addData("Position", yaw);
-        telemetry.addData("second before", second);
-        telemetry.addData("second after", convertify(second));
-        telemetry.update();
-        }
-        frontleft.setPower(0);
-        frontright.setPower(0);
-        backleft.setPower(0);
-        backright.setPower(0);
+            while (!(seconda < yaw && yaw < secondb) && opModeIsActive()) {//within range?
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+                yaw = -angles.firstAngle;
+                telemetry.addData("Position", yaw);
+                telemetry.addData("second before", second);
+                telemetry.addData("second after", convertify(second));
+                telemetry.update();
+            }
+            while (!((seconda < yaw && yaw < 180) || (-180 < yaw && yaw < secondb)) && opModeIsActive()) {//within range?
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+                yaw = -angles.firstAngle;
+                telemetry.addData("Position", yaw);
+                telemetry.addData("second before", second);
+                telemetry.addData("second after", convertify(second));
+                telemetry.update();
+            }
+            frontleft.setPower(0);
+            frontright.setPower(0);
+            backleft.setPower(0);
+            backright.setPower(0);
         }
         //</editor-fold>
         //
@@ -207,13 +206,13 @@ public void turnWithGyro(double degrees, double speedDirection){
         frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }
-//
+    }
+    //
     /*
     This function uses the encoders to strafe left or right.
     Negative input for inches results in left strafing.
      */
-public void strafeToPosition(double inches, double speed){
+    public void strafeToPosition(double inches, double speed){
         //
         int move = (int)(Math.round(inches * cpi * meccyBias));
         //
@@ -237,42 +236,43 @@ public void strafeToPosition(double inches, double speed){
         frontleft.setPower(0);
         backright.setPower(0);
         backleft.setPower(0);
-        }
-//
+        return;
+    }
+    //
     /*
     A tradition within the Thunder Pengwins code, we always start programs with waitForStartify,
     our way of adding personality to our programs.
      */
-public void waitForStartify(){
+    public void waitForStartify(){
         waitForStart();
-        }
-//
+    }
+    //
     /*
     These functions are used in the turnWithGyro function to ensure inputs
     are interpreted properly.
      */
-public double devertify(double degrees){
+    public double devertify(double degrees){
         if (degrees < 0){
-        degrees = degrees + 360;
+            degrees = degrees + 360;
         }
         return degrees;
-        }
-public double convertify(double degrees){
+    }
+    public double convertify(double degrees){
         if (degrees > 179){
-        degrees = -(360 - degrees);
+            degrees = -(360 - degrees);
         } else if(degrees < -180){
-        degrees = 360 + degrees;
+            degrees = 360 + degrees;
         } else if(degrees > 360){
-        degrees = degrees - 360;
+            degrees = degrees - 360;
         }
         return degrees;
-        }
-//
+    }
+    //
     /*
     This function is called at the beginning of the program to activate
     the IMU Integrated Gyro.
      */
-public void initGyro(){
+    public void initGyro(){
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
@@ -283,13 +283,13 @@ public void initGyro(){
         //
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
-        }
-//
+    }
+    //
     /*
     This function is used in the turnWithGyro function to set the
     encoder mode and turn.
      */
-public void turnWithEncoder(double input){
+    public void turnWithEncoder(double input){
         frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -299,6 +299,7 @@ public void turnWithEncoder(double input){
         backleft.setPower(input);
         frontright.setPower(-input);
         backright.setPower(-input);
-        }
-        //
-        }
+    }
+    //
+}
+
