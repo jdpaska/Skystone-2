0e254b698a67c1a19a6839d8e4d524d0fb1f9e32
CapitolHillFTC <CapitolHillFTC@gmail.com>
2019-12-02 09:11:57 -0600

    deleted extra debug messages

--- Team10733/src/main/java/org/firstinspires/ftc/team10733/ArmController.java
+++ Team10733/src/main/java/org/firstinspires/ftc/team10733/ArmController.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.team10733;
 
 public class ArmController {
 
+
+
 public void extend() {
     //extend the arm
 
--- Team10733/src/main/java/org/firstinspires/ftc/team10733/LZ1.java
+++ Team10733/src/main/java/org/firstinspires/ftc/team10733/LZ1.java
@@ -13,7 +13,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name="LZ 1", group="chad")
+@Autonomous(name="AutoRedByCenter", group="chad")
 public class LZ1 extends LinearOpMode {
         //
         DcMotor frontleft;
