b38825b7e643142ccf55c81b3739d7ef5326e042
jdpaska <35551227+jdpaska@users.noreply.github.com>
2019-12-03 18:28:46 -0600

    declarations in ArmController

--- Team10733/src/main/java/org/firstinspires/ftc/team10733/ArmController.java
+++ Team10733/src/main/java/org/firstinspires/ftc/team10733/ArmController.java
@@ -1,11 +1,27 @@
 package org.firstinspires.ftc.team10733;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class ArmController {
 
+    //define the arm devices
+    private Servo leftServo;
+    private Servo  rightServo;
+    private DcMotor armMotor = null;
 
+    //set min and max servo positions
+    static final double LEFT_MAX_POS     =  1.0;     // Maximum rotational position
+    static final double LEFT_MIN_POS     =  0.0;     // Minimum rotational position
+    static final double RIGHT_MAX_POS     =  1.0;     // Maximum rotational position
+    static final double RIGHT_MIN_POS     =  0.0;     // Minimum rotational position
+
+    double  leftServoPosition = .5; // left servo start position
+    double  rightServoPosition = .5; // right servo start position
 
 public void extend() {
     //extend the arm
+
 
 }
 
